8c0bdc5548b2ddf559efd65dc20b84b081f8815c
Reed <reedfodge@gmail.com>
2019-04-14 01:58:27 -0400

    Updated fileConverter

--- fileConverter.py
+++ fileConverter.py
@@ -6,7 +6,7 @@ from os.path import isfile, join
 from tabula import convert_into
 
 #Path where spreadsheets are located
-path = '/Users/reed/Desktop/testSpreadsheets'
+path = '/Users/reed/Desktop/Spreadsheets'
 #Gets every file in path and stores in list
 files = [f for f in listdir(path) if isfile(join(path, f))]
 
@@ -27,8 +27,12 @@ def convertXLS(a):
     csv_file = open(nameWithoutExtension, 'wb')
     wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
     for rownum in xrange(sh.nrows):
-        wr.writerow(sh.row_values(rownum))
-    print("CONVERTED FILE " + a + " TO .CSV")
+        try:
+            wr.writerow(sh.row_values(rownum))
+        except UnicodeEncodeError:
+            print("UNICODE ENCODE ERROR (FIX)")
+    print("CONVERTED XLS(X) FILE " + a + " TO .CSV")
+    os.remove(fileName)
     csv_file.close()
 
 #Converts a .pdf file to a .csv
@@ -36,8 +40,10 @@ def convertPDF(a):
     fileName = os.path.join(path, a)
     csvFileName = a[0:len(a)-4]
     convert_into(fileName, csvFileName, output_format="csv")
+    print("CONVERTED PDF FILE " + a + " TO .CSV")
+    os.remove(fileName)
 
-
+#Goes through all the files and converts them if necessary
 for a in files:
     extension = ''
     #Identifies type of file
